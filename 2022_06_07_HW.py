5741454f1f5b69a98aeef69fa2be60f540ecd7af
Dan Pyrogov <danpyrogov@gmail.com>
2022-06-07 01:27:43 +0300

    20220607

---
+++ 2022_06_07_HW.py
@@ -0,0 +1,12 @@
+dict_homework = {
+    "key1":{
+        "fff1":"sss",
+        "f":"sss",
+        "s":["fff", "GET ME"],
+    },
+    "key2":"value2",
+    "key3":"value3",
+    "key4":"value4",
+}
+
+print(dict_homework["key1"]["s"][1])
